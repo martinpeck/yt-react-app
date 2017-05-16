18645bd29205db967f3a6a77e8611215ce62ecc6
Martin Peck <mpeck@hotmail.co.uk>
2017-05-16 23:07:10 +0100

    Added youtube API package and start of SearchBar component

--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+import SearchBar from './components/search_bar'
+
+const API_KEY = 'AIzaSyA8HzvAGOyLMgb-Yo5m2Y2u5I9vxSXmFJQ';
+
 // create a new component
 const App = () => {
-    return <div>Hello, world!</div>;
+    return (
+    <div>
+        <SearchBar />
+    </div>
+    );
+
 }
 
 // take this component's generated html and inject into the DOM
