f07946aba8b75edd2f75074636b75affbcff71e1
Martin Peck <mpeck@hotmail.co.uk>
2017-05-16 22:24:01 +0100

    Hello world implemented

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
 // create a new component
+const App = () => {
+    return <div>Hello, world!</div>;
+}
 
 // take this component's generated html and inject into the DOM
+ReactDOM.render(<App />, document.querySelector('.container'))
