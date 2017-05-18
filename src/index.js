ec324db9f78d819ce68978331716a60eb3706d96
Martin Peck <mpeck@hotmail.co.uk>
2017-05-18 22:00:14 +0100

    refactor app to class based

--- src/index.js
+++ src/index.js
@@ -1,24 +1,30 @@
-import React from 'react'
+import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
 import YTSearch from 'youtube-api-search'
-
 import SearchBar from './components/search_bar'
 
 const API_KEY = 'AIzaSyA8HzvAGOyLMgb-Yo5m2Y2u5I9vxSXmFJQ';
 
-YTSearch({ key: API_KEY, term: 'surfboards'}, function(data){
-    console.log(data);
-})
 
-// create a new component
-const App = () => {
-    return (
-    <div>
-        <SearchBar />
-    </div>
-    );
+class App extends Component {
+    
+    constructor(props) {
+        super(props);
 
+        this.state = { videos: []};
+
+        YTSearch({ key: API_KEY, term: 'surfboards'}, (videos) => {
+            this.setState({ videos });
+       });
+    }
+
+    render() {
+        return (
+            <div>
+                <SearchBar />
+            </div>
+        );
+    }
 }
 
-// take this component's generated html and inject into the DOM
 ReactDOM.render(<App />, document.querySelector('.container'))
