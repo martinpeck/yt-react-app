cce3c33d84e3f729d5fddb487d1ef532d65078b4
Martin Peck <mpeck@hotmail.co.uk>
2017-05-18 22:07:40 +0100

    Add basic VIdeoList

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
 import YTSearch from 'youtube-api-search'
 import SearchBar from './components/search_bar'
+import VideoList from './components/video_list'
 
 const API_KEY = 'AIzaSyA8HzvAGOyLMgb-Yo5m2Y2u5I9vxSXmFJQ';
 
@@ -22,6 +23,7 @@ class App extends Component {
         return (
             <div>
                 <SearchBar />
+                <VideoList videos= {this.state.videos} />
             </div>
         );
     }
