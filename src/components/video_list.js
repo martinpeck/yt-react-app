b869057be1bdeec6b49e95309a54f5c8f87032cc
Martin Peck <mpeck@hotmail.co.uk>
2017-05-18 22:50:33 +0100

    refactor VideoListItem

--- src/components/video_list.js
+++ src/components/video_list.js
@@ -3,10 +3,10 @@ import VideoListItem from './video_list_item';
 
 const VideoList = (props) => {
 
-    const videoItems = props.videos.map((video) => {
-        return <VideoListItem video={video} />
+    const videoItems = props.videos.map((video) => {    
+        return <VideoListItem key={video.id.videoId} video={video} />
     });
-
+    
     return (
         <ul className="col-md-4 list-group">
             { videoItems }
